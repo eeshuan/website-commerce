27fac22ff74111d8dbda1261c513fe3756016387
eeshuan <ngeeshuan@hotmail.com>
2020-05-03 15:06:12 +0800

    Added picker filters in store page

--- src/pages/store/store.tsx
+++ src/pages/store/store.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import Layout from '../layout/layout';
-import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Grid } from '@material-ui/core';
+import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Grid, Checkbox, FormControlLabel } from '@material-ui/core';
 import ItemCard from '../../components/react/itemCard/itemCard';
 import Item from '../../scripts/class/item';
+import Slider from '@material-ui/core/Slider';
 import { testdata } from '../../testdata/testdata';
 
 import './store.scss';
 
 export default class Store extends React.Component {
-    private testItems: Array<Item>;
+    private testItems: Array<Item>
+
+    private readonly PRICE_LOW: number = 0;
+    private readonly PRICE_MEDIUM: number = 1;
+    private readonly PRICE_HIGH: number = 2;
+
+    private readonly priceMarks = [
+        {
+            value: this.PRICE_LOW,
+            label: `Low`
+        },
+        {
+            value: this.PRICE_MEDIUM,
+            label: `Medium`
+        },
+        {
+            value: this.PRICE_HIGH,
+            label: `High`
+        },
+    ];
+
+    state = {
+        filteredItems: [],
+        priceRange: [this.PRICE_LOW, this.PRICE_HIGH]
+    }
 
     constructor(public props) {
         super(props);
@@ -16,7 +41,25 @@ export default class Store extends React.Component {
         Object.keys(testdata.items).map((key: string, index: number) => {
             let item: {name: string, price: number, imageUrl: string} = testdata.items[key];
             this.testItems[index] = new Item(key, item.name, item.price, item.imageUrl, key);
+            this.state.filteredItems[index] = this.testItems[index];
         });
+    }
+
+    private handlePriceChange(ev: any, newValue: number[]) {
+        this.setState({priceRange: newValue});
+    }
+
+    private getItemsWithFilter() {
+        console.log(this.state.filteredItems);
+        return (
+            this.state.filteredItems.map((data: Item, index: number) => {
+                return (
+                    <Grid className="store-items-itemcontainer" item md={4} sm={6} xs={12} key={index}>
+                        <ItemCard item={data}/>
+                    </Grid>
+                );
+            })
+        );
     }
 
     render () {
@@ -31,7 +74,15 @@ export default class Store extends React.Component {
                                         <div>Price</div>
                                     </ExpansionPanelSummary>
                                     <ExpansionPanelDetails className="store-navigation-panel-details">
-                                        <div>Price slider here</div>
+                                        <Slider
+                                            classes={{root: "store-navigation-panel-priceslider"}}
+                                            value={this.state.priceRange}
+                                            onChange={this.handlePriceChange.bind(this)}
+                                            marks={this.priceMarks}
+                                            min={this.PRICE_LOW}
+                                            max={this.PRICE_HIGH}
+                                            aria-labelledby="discrete-range-slider"
+                                        />
                                     </ExpansionPanelDetails>
                                 </ExpansionPanel>
                                 <ExpansionPanel className="store-navigation-panel">
@@ -39,7 +90,52 @@ export default class Store extends React.Component {
                                         <div>Colour</div>
                                     </ExpansionPanelSummary>
                                     <ExpansionPanelDetails className="store-navigation-panel-details">
-                                        <div>Colour picker here</div>
+                                        <div className="store-navigation-panel-colourpicker">
+                                            <FormControlLabel
+                                                control={<Checkbox name="checkbox_red"/>}
+                                                label={
+                                                    <span>
+                                                        <span>Red</span>
+                                                        <span
+                                                            className="store-navigation-panel-colourpicker-colour"
+                                                            style={{backgroundColor: '#ff0000'}}></span>
+                                                    </span>
+                                                }
+                                            />
+                                            <FormControlLabel
+                                                control={<Checkbox name="checkbox_blue"/>}
+                                                label={
+                                                    <span>
+                                                        <span>Blue</span>
+                                                        <span
+                                                            className="store-navigation-panel-colourpicker-colour"
+                                                            style={{backgroundColor: '#0000ff'}}></span>
+                                                    </span>
+                                                }
+                                            />
+                                            <FormControlLabel
+                                                control={<Checkbox name="checkbox_green"/>}
+                                                label={
+                                                    <span>
+                                                        <span>Green</span>
+                                                        <span
+                                                            className="store-navigation-panel-colourpicker-colour"
+                                                            style={{backgroundColor: '#00ff00'}}></span>
+                                                    </span>
+                                                }
+                                            />
+                                            <FormControlLabel
+                                                control={<Checkbox name="checkbox_yellow"/>}
+                                                label={
+                                                    <span>
+                                                        <span>Yellow</span>
+                                                        <span
+                                                            className="store-navigation-panel-colourpicker-colour"
+                                                            style={{backgroundColor: '#ffff00'}}></span>
+                                                    </span>
+                                                }
+                                            />
+                                        </div>
                                     </ExpansionPanelDetails>
                                 </ExpansionPanel>
                                 <ExpansionPanel className="store-navigation-panel">
@@ -47,23 +143,32 @@ export default class Store extends React.Component {
                                         <div>Sizes</div>
                                     </ExpansionPanelSummary>
                                     <ExpansionPanelDetails className="store-navigation-panel-details">
-                                        <div>Size picker here</div>
+                                        <div className="store-navigation-panel-sizepicker">
+                                            <FormControlLabel
+                                                control={<Checkbox name="checkbox_xs"/>}
+                                                label="XS"
+                                            />
+                                            <FormControlLabel
+                                                control={<Checkbox name="checkbox_s"/>}
+                                                label="S"
+                                            />
+                                            <FormControlLabel
+                                                control={<Checkbox name="checkbox_m"/>}
+                                                label="M"
+                                            />
+                                            <FormControlLabel
+                                                control={<Checkbox name="checkbox_l"/>}
+                                                label="L"
+                                            />
+                                        </div>
                                     </ExpansionPanelDetails>
                                 </ExpansionPanel>
                             </div>
                         </Grid>
                         <Grid item xs={10}>
                             <div className="store-items">
-                                <Grid container alignItems="flex-end" justify="flex-end">
-                                    {
-                                        this.testItems.map((data: Item, index: number) => {
-                                            return (
-                                                <Grid className="store-items-itemcontainer" item md={4} sm={6} xs={12} key={index}>
-                                                    <ItemCard item={data}/>
-                                                </Grid>
-                                            );
-                                        })
-                                    }
+                                <Grid container alignItems="flex-start" justify="flex-start">
+                                    {this.getItemsWithFilter()}
                                 </Grid>
                             </div>
                         </Grid>
--- src/testdata/testdata.ts
+++ src/testdata/testdata.ts
@@ -3,48 +3,75 @@ import { assetsMap } from "../assets/assetsMap";
 export let testdata = {
     items: {
         "item-1": {
-            name: "Item 1",
+            id: `item-1`,
+            name: `Item 1`,
             price: 10,
+            colour: `red`,
+            size: `xs;s;m`,
             imageUrl: assetsMap.pages.store.item
         },
         "item-2": {
-            name: "Item 2",
-            price: 11,
+            id: `item-2`,
+            name: `Item 2`,
+            price: 20,
+            colour: `blue`,
+            size: `s;m`,
             imageUrl: assetsMap.pages.store.item
         },
         "item-3": {
-            name: "Item 3",
-            price: 12,
+            id: `item-3`,
+            name: `Item 3`,
+            price: 250,
+            colour: `green`,
+            size: `m;l`,
             imageUrl: assetsMap.pages.store.item
         },
         "item-4": {
-            name: "Item 4",
-            price: 13,
+            id: `item-4`,
+            name: `Item 4`,
+            price: 70,
+            colour: `yellow`,
+            size: `xs;s;m;l`,
             imageUrl: assetsMap.pages.store.item
         },
         "item-5": {
-            name: "Item 5",
-            price: 14,
+            id: `item-5`,
+            name: `Item 5`,
+            price: 20,
+            colour: `red`,
+            size: `m;l`,
             imageUrl: assetsMap.pages.store.item
         },
         "item-6": {
-            name: "Item 6",
-            price: 15,
+            id: `item-6`,
+            name: `Item 6`,
+            price: 120,
+            colour: `green`,
+            size: `xs;s;m;l`,
             imageUrl: assetsMap.pages.store.item
         },
         "item-7": {
-            name: "Item 7",
-            price: 16,
+            id: `item-7`,
+            name: `Item 7`,
+            price: 50,
+            colour: `blue`,
+            size: `xs;s;m`,
             imageUrl: assetsMap.pages.store.item
         },
         "item-8": {
-            name: "Item 8",
-            price: 17,
+            id: `item-8`,
+            name: `Item 8`,
+            price: 140,
+            colour: `yellow`,
+            size: `s;m;l`,
             imageUrl: assetsMap.pages.store.item
         },
         "item-9": {
-            name: "Item 9",
-            price: 18,
+            id: `item-9`,
+            name: `Item 9`,
+            price: 190,
+            colour: `red`,
+            size: `s;m;l`,
             imageUrl: assetsMap.pages.store.item
         }
     }
